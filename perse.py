01306aaad6dda7fb190e0a4e909bf69043c28ee1
RyckLima <henryquelima000@gmail.com>
2026-02-19 18:10:50 -0300

    feat: adição de POLLING adicionado esse polling para poder resolver o problema do Assincronismo da API que estamos recebendo dados,para garantir que vamos receber os dados.

--- perse.py
+++ perse.py
@@ -21,25 +21,33 @@ with open(path , 'rb') as f:
 job_id  = response.json()['id']
 path2 = os.path.join('outputs' , f'{job_id}.json')
 
-
+#Polling (para resolver o assincronismo na API da web)
 while True:
-    response2 = requests.get(url = f'https://api.cloud.llamaindex.ai/api/v1/parsing/job/{job_id}',
+    response_text = requests.get(url = f'https://api.cloud.llamaindex.ai/api/v1/parsing/job/{job_id}/result/text',
                             headers = {
                             'accept' : 'application/json' ,
                             'Authorization' : f'Bearer {api_key}' }
                             )
-    
-    dados = response2.json()
-    status = dados.get('status')
-    print(dados)
+    response_metadata = requests.get(url = f'https://api.cloud.llamaindex.ai/api/v1/parsing/job/{job_id}/result/json',
+                            headers = {
+                            'accept' : 'application/json' ,
+                            'Authorization' : f'Bearer {api_key}' }
+                            )
 
-    if status == 'SUCCESS': # Ou o status final indicado na doc da v2
-        # SÓ AQUI as chaves 'text' e 'metadata' vão existir!
-        texto = dados.get('text')
+
+    dados_text = response_text.json()
+    dados_metadata = response_metadata.json()
+    status1 = dados_text.get('status')
+    status2 = dados_metadata.get('status')
+    print(dados_text)
+
+    if status1 == 'SUCCESS' and status2 == 'SUCCESS' : #se os status tiver ok pode prosseguir
+        texto = dados_text.get('text')
+        metadados = dados_metadata.get('job_metadata')
         with open( path2 , 'w') as f:
-            f.write(f' Aqui estão os dados: {texto}')
+            f.write(f'Aqui está os metadados: Aqui está o texto: {texto}\n')
         break
-    elif status == 'FAILED':
+    elif status1 == 'FAILED' or status2 == 'FAILED' :
         print("Erro no processamento")
         break
     
