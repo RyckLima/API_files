f49a490e3683ce264d755a4f990b79dc8a1d6b62
RyckLima <henryquelima000@gmail.com>
2026-02-19 15:31:34 -0300

    finalizar as requisições

--- perse.py
+++ perse.py
@@ -17,9 +17,16 @@ with open(path , 'rb') as f:
                     file = f
                     )
 
-response2 = requests.get(url = 'https://api.cloud.llamaindex.ai/api/v2/parse/:job_id',
+job_id  = response.json()['id']
+path2 = os.path.join('uploads' , f'{job_id}.json')
+
+response2 = requests.get(url = f'https://api.cloud.llamaindex.ai/api/v2/parse/{job_id}',
                         headers = {
                         'accept' : 'application/json' ,
                         'Authorization' : f'Bearer {api_key}' }
                         )
+
+with open( path2 , 'w') as f:
+    f.write(f'Aqui estão os metadados: {response2.metadata}\n Aqui estão os dados: {response2.text}')
+
 
