46f30f6326c632346ea7ff98799cfaa54b44d049
RyckLima <henryquelima000@gmail.com>
2026-02-19 18:28:38 -0300

    fix: correção do polling eu estava acessando as chaves status onde nao existia, o correto era so o status do job Please enter the commit message for your changes. Lines starting

--- perse.py
+++ perse.py
@@ -5,13 +5,13 @@ import time
 file_name = 'SIMITA1aFASE-07022026-C0.pdf'
 path = os.path.join('uploads' , file_name)
 api_key = 'llx-rW7ACedLCrKxe07hcxTOyg090kRSKZhdr96TACPDyNdqOoIC'
+headers = {'accept' : 'application/json' ,
+           'Authorization' : f'Bearer {api_key}' }
 
 with open(path , 'rb') as f:
     
     response = requests.post(url = 'https://api.cloud.llamaindex.ai/api/v1/parsing/upload', 
-                    headers = {
-                        'accept' : 'application/json' ,
-                        'Authorization' : f'Bearer {api_key}' },
+                    headers = headers,
                     data = {
                         'language' : 'pt'},
                     files = {'file' : f}
@@ -23,31 +23,30 @@ path2 = os.path.join('outputs' , f'{job_id}.json')
 
 #Polling (para resolver o assincronismo na API da web)
 while True:
+
+    response_job = requests.get(url = f'https://api.cloud.llamaindex.ai/api/v1/parsing/job/{job_id}',
+                            headers = headers
+                            )
     response_text = requests.get(url = f'https://api.cloud.llamaindex.ai/api/v1/parsing/job/{job_id}/result/text',
-                            headers = {
-                            'accept' : 'application/json' ,
-                            'Authorization' : f'Bearer {api_key}' }
+                            headers = headers
                             )
     response_metadata = requests.get(url = f'https://api.cloud.llamaindex.ai/api/v1/parsing/job/{job_id}/result/json',
-                            headers = {
-                            'accept' : 'application/json' ,
-                            'Authorization' : f'Bearer {api_key}' }
+                            headers = headers
                             )
 
 
+    status  = response_job.json().get('status')
     dados_text = response_text.json()
     dados_metadata = response_metadata.json()
-    status1 = dados_text.get('status')
-    status2 = dados_metadata.get('status')
-    print(dados_text)
+    print(status)
 
-    if status1 == 'SUCCESS' and status2 == 'SUCCESS' : #se os status tiver ok pode prosseguir
+    if status == 'SUCCESS' : #se os status tiver ok pode prosseguir
         texto = dados_text.get('text')
         metadados = dados_metadata.get('job_metadata')
         with open( path2 , 'w') as f:
-            f.write(f'Aqui está os metadados: Aqui está o texto: {texto}\n')
+            f.write(f'Aqui está os metadados:{metadados} Aqui está o texto: {texto}\n')
         break
-    elif status1 == 'FAILED' or status2 == 'FAILED' :
+    elif status == 'FAILED' :
         print("Erro no processamento")
         break
     
